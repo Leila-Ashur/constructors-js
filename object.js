1f888d19e65e026e03d9b7e58226acdde0fccca0
leilaashur <leilahashur@gmail.com>
2023-05-02 13:01:12 +0300

    constructors commit

--- object.js
+++ object.js
@@ -1,5 +1,7 @@
-// Given an array of objects, each object representing a person with a name and age property, 
-// write a function that returns a new array containing the names of all people who are 18 years old or older.
+// Given an array of objects, each object representing a person
+//  with a name and age property,  write a function that
+//  returns a new array containing the names of all people
+//  who are 18 years old or older.
 const people = [
     { name: 'Alice', age: 17 },
     { name: 'Eunice', age: 22 },
@@ -16,10 +18,21 @@ const people = [
       return olderArr
   }
   console.log(adults(people))
-  // Write a function that takes an array of objects, where each object represents a product
-  //  with a name, price, and category property. 
-  // The function should return an object that groups the products by their categories, 
-  // with the category names as keys and the arrays of products as values.
+
+
+  function getNames(people) {
+    let adults=people.filter(person=>person>=18). map(person=>
+      person.name)
+      return adults
+
+    
+  }
+  // Write a function that takes an array of objects, where
+  //  each object represents a productwith a name, price, and
+  //  category property.  The function should return an object
+  //  that groups the products by their categories,  with
+  //  the category names as keys and the arrays of products 
+  // as values.
   
   function category(products) {
     const groups = {};
@@ -39,6 +52,22 @@ const people = [
   ];
   const groupedProducts = category(products);
   console.log(groupedProducts);
+
+
+  // 
+  const group=(product)=>{
+    let groupedProducts= {}
+    products.forEach(item=>{
+     if(!item.hasownProperty(category)){
+       groupedProducts.category=[];
+
+     } 
+     else{
+       groupedProducts[category].push(item);
+     }
+     return groupedProducts
+    })
+  }
   // Given an array of objects, where each object represents a student with a name and an array of scores, 
   // write a function that returns a new array containing the names of all students whose average score is greater than or equal to 85.
   // const students = [
@@ -73,6 +102,16 @@ const people = [
   const highPerformers = topStudents(students);
   
   console.log(highPerformers); 
+
+
+
+   const highestScores=(student)=>{
+     students.forEach(item=>{
+       const scores=item.scores
+       const sum = scores.reduce (a,b)
+       const average=sum
+     })
+  }
     
     // Given an object representing a car, with properties for the make, model, year, and a method to 
   // display the car's information, 
